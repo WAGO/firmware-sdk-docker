2874b5c92f79cae5ed72370a14b3660e79571865
Dirk Meihoefer <dirk.meihoefer@wago.com>
2022-01-21 14:24:43 +0000

    Added JFROG_APIKEY

--- .devcontainer/resources/docker-entrypoint.sh
+++ .devcontainer/resources/docker-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git lfs pull
-export JFROG_APIKEY="$(cat /secrets/jfrog-api-key)"
 exec /usr/local/share/docker-init.sh "$@"
---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export JFROG_APIKEY=$(cat /secrets/jfrog-api-key)
+
