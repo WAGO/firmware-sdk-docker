4c7d40aba263122d366b9a871af6449b4668cab6
Dirk Meihoefer <dirk.meihoefer@wago.com>
2022-06-28 13:09:12 +0200

    Added init.sh.

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+TAG=V03.09.07_21
+
 export JFROG_APIKEY=$(cat /secrets/jfrog-api-key)
+
+svn checkout https://svsv01003.wago.local/svn/repo3/pfc/tags/wago-pfc200/$TAG/ wago-src/$TAG
+
+cd /workspaces/firmware-sdk-docker/wago-src/$TAG/ptxproj
+ptxdist platform configs/wago-pfcXXX/platformconfig 
+ptxdist select configs/wago-pfcXXX/ptxconfig_pfc_g2
+ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
 
