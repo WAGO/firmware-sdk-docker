7be1f65b193e0d01fb87e7c7dec0a70d329b62b3
Dirk Meihoefer <dirk.meihoefer@wago.com>
2022-07-15 08:32:33 +0000

    Updated build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@ set -e
 
 USER=$1
 PASSWORD=$2
-SVN_TAG=${3:-V03.09.07_21}
-IMAGE_TAG=${4:-FW21-03.09.07}
+SVN_TAG=V03.09.07_21
+IMAGE_TAG=FW21-03.09.07
 
 IMAGE_NAME=wagoautomation/pfc-firmware-base
 
 .devcontainer/scripts/init-subversion-src.sh $SVN_TAG $USER $PASSWORD 
 cd src/$SVN_TAG/ptxproj
-# ptxdist images -q -j
+ptxdist images -q -j
 
 cd /workspaces
-docker build --platform=linux/arm/v7 --build-arg SOURCE_DIR=$SVN_TAG  -t $IMAGE_NAME:$IMAGE_TAG .
+docker build --platform=linux/arm/v7 --label "org.label-schema.vcs-ref=$(gitversion -showvariable ShortSha)" --build-arg SOURCE_DIR=$SVN_TAG  -t $IMAGE_NAME:$IMAGE_TAG .
